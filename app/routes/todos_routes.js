677d7491b89093a6b810d01264da46f819e7b2e0
bahmbies <y.banana12@gmail.com>
2020-11-06 12:11:31 -0600

    init-commit

---
+++ app/routes/todos_routes.js
@@ -0,0 +1,115 @@
+// Express docs: http://expressjs.com/en/api.html
+const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
+const passport = require('passport')
+
+// pull in Mongoose model for todos
+const Todo = require('../models/todo')
+
+// this is a collection of methods that help us detect situations when we need
+// to throw a custom error
+const customErrors = require('../../lib/custom_errors')
+
+// we'll use this function to send 404 when non-existant document is requested
+const handle404 = customErrors.handle404
+// we'll use this function to send 401 when a user tries to modify a resource
+// that's owned by someone else
+const requireOwnership = customErrors.requireOwnership
+
+// this is middleware that will remove blank fields from `req.body`, e.g.
+// { todo: { title: '', text: 'foo' } } -> { todo: { text: 'foo' } }
+const removeBlanks = require('../../lib/remove_blank_fields')
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `req.user`
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+// INDEX
+// GET /todos
+router.get('/todos', requireToken, (req, res, next) => {
+  Todo.find()
+    .then(todos => {
+      // `todos` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return todos.map(todo => todo.toObject())
+    })
+    // respond with status 200 and JSON of the todos
+    .then(todos => res.status(200).json({ todos: todos }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// SHOW
+// GET /todos/5a7db6c74d55bc51bdf39793
+router.get('/todos/:id', requireToken, (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  Todo.findById(req.params.id)
+    .then(handle404)
+    // if `findById` is succesful, respond with 200 and "todo" JSON
+    .then(todo => res.status(200).json({ todo: todo.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// CREATE
+// POST /todos
+router.post('/todos', requireToken, (req, res, next) => {
+  // set owner of new todo to be current user
+  req.body.todo.owner = req.user.id
+
+  Todo.create(req.body.todo)
+    // respond to succesful `create` with status 201 and JSON of new "todo"
+    .then(todo => {
+      res.status(201).json({ todo: todo.toObject() })
+    })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
+    .catch(next)
+})
+
+// UPDATE
+// PATCH /todos/5a7db6c74d55bc51bdf39793
+router.patch('/todos/:id', requireToken, removeBlanks, (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.todo.owner
+
+  Todo.findById(req.params.id)
+    .then(handle404)
+    .then(todo => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, todo)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return todo.updateOne(req.body.todo)
+    })
+    // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// DESTROY
+// DELETE /todos/5a7db6c74d55bc51bdf39793
+router.delete('/todos/:id', requireToken, (req, res, next) => {
+  Todo.findById(req.params.id)
+    .then(handle404)
+    .then(todo => {
+      // throw an error if current user doesn't own `todo`
+      requireOwnership(req, todo)
+      // delete the todo ONLY IF the above didn't throw
+      todo.deleteOne()
+    })
+    // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+module.exports = router
---
+++ curl-scripts/todos/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/todos"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "todo": {
+      "text": "'"${TEXT}"'",
+      "title": "'"${TITLE}"'"
+    }
+  }'
+
+echo
